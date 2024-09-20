feadf55463c2bc42c3239cac102a651026ff3973
huanciou <huanciou@github.com>
2024-09-20 19:34:17 +0800

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -25,19 +25,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	defer conn.Close()
-
 	buffer := make([]byte, 1024)
 
 	if _, err := conn.Read(buffer); err != nil {
 		fmt.Println("conn Read error: ", err.Error())
 	} else {
-		fmt.Println("conn Read success")
+		fmt.Println(string(buffer))
 	}
 
-	if _, err := conn.Write([]byte{0, 0, 0, 0, 0, 0, 0, 7}); err != nil {
+	//	The request starts with a 4 byte length field
+	//	buffer[0:4] -> request start string / 4 BYTE
+	//	buffer[4:6] -> request_api_key	INT16 / 2 BYTE
+	//	buffer[6:8] -> request_api_version	INT16 / 2 BYTE
+	//	buffer[8:12] -> correlation_id	INT32 / 4 BYTE
+
+	resp := make([]byte, 8)
+	copy(resp[:4], []byte{0, 0, 0, 0})
+	copy(resp[4:], buffer[8:12])
+
+	if _, err := conn.Write(resp); err != nil {
 		fmt.Println("conn Write error: ", err.Error())
 	} else {
 		fmt.Println("conn Write success")
 	}
+
+	defer conn.Close()
+
 }
